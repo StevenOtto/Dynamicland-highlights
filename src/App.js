ab942f37f37938f8ff39a06f42cd601ba2be75b2
Steven Otto <steven.otto@in10.nl>
2024-09-30 14:03:40 +0200

    add new files

--- src/App.js
+++ src/App.js
@@ -1,161 +1,74 @@
 // Styles
 import "./tailwind.output.css";
+import "./fonts.css";
 import React, { useState } from "react";
 
 const topics = [
   {
     id: "intro",
-    title: "Introduction",
-    url: "https://dynamicland.org/",
-    subtopics: [
-      {
-        id: "intro-overview",
-        title: "Brief overview of Dynamicland and Realtalk",
-        url: "https://dynamicland.org/about/",
-      },
-      {
-        id: "intro-relevance",
-        title: "Relevance to design and technology",
-        url: "https://dynamicland.org/vision/",
-      },
-    ],
-  },
-  {
-    id: "dynamicland",
-    title: "Dynamicland",
-    url: "https://dynamicland.org/about/",
-    subtopics: [
-      {
-        id: "dynamicland-concept",
-        title: "Concept and vision",
-        url: "https://dynamicland.org/vision/",
-      },
-      {
-        id: "dynamicland-principles",
-        title: "Key principles",
-        url: "https://dynamicland.org/principles/",
-      },
-      {
-        id: "dynamicland-examples",
-        title: "Examples of Dynamicland projects",
-        url: "https://dynamicland.org/projects/",
-      },
-    ],
+    title: "Website",
+    url: "https://dynamicland.org",
+    subtopics: [],
   },
   {
     id: "realtalk",
-    title: "Realtalk",
-    url: "https://www.youtube.com/embed/RhL34DP-2Ns",
+    title: "Realtalk OS",
+    url: "https://www.youtube.com/embed/uI7J3II59lc?si=XlYjJmZB8NbCsqs5&amp;start=1885",
     subtopics: [
+      {
+        id: "dynamicland-principles",
+        title: "Motivatie",
+        url: "https://www.youtube.com/embed/cErKuEHWCpM?si=vtrCIrBU1yFP6aNP&amp;start=302",
+      },
       {
         id: "realtalk-what",
-        title: "What is Realtalk?",
-        url: "https://www.youtube.com/embed/RhL34DP-2Ns?start=60",
+        title: "Hoe werkt het?",
+        url: "https://www.youtube.com/embed/cErKuEHWCpM?si=zUnc56T3xiewpH_P&amp;start=735",
       },
       {
-        id: "realtalk-differences",
-        title: "How it differs from traditional programming",
-        url: "https://www.youtube.com/embed/RhL34DP-2Ns?start=180",
-      },
-      {
-        id: "realtalk-role",
-        title: "Its role in Dynamicland's ecosystem",
-        url: "https://www.youtube.com/embed/RhL34DP-2Ns?start=300",
+        id: "realtalk-how",
+        title: "Programma's schrijven",
+        url: "https://www.youtube.com/embed/cErKuEHWCpM?si=RJZn2etOJCsGIqhP&amp;start=1666",
       },
     ],
   },
   {
-    id: "implications",
-    title: "Implications for Design",
-    url: "https://medium.com/@nolac/reflections-on-dynamicland-65158b06196",
+    id: "demo",
+    title: "Demo",
+    url: "https://www.youtube.com/embed/5Q9r-AEzRMA?si=EZG_WFLEqPgIlHH9&amp;start=12",
+    subtopics: [],
+  },
+  {
+    id: "exammples",
+    title: "Voorbeelden",
+    url: "#",
     subtopics: [
       {
-        id: "implications-ui",
-        title: "New ways of thinking about user interfaces",
-        url: "https://medium.com/@nolac/reflections-on-dynamicland-65158b06196#:~:text=New%20ways%20of%20thinking",
+        id: "game-design",
+        title: "Game design",
+        url: "https://www.youtube.com/embed/eJm44LJDU44?si=FB1WvjQOp8ey6tAO&amp;start=1517",
       },
       {
-        id: "implications-bridge",
-        title: "Bridging digital and physical worlds",
-        url: "https://medium.com/@nolac/reflections-on-dynamicland-65158b06196#:~:text=Bridging%20digital%20and%20physical",
+        id: "bookcase",
+        title: "Interactieve boekenkast",
+        url: "https://www.youtube.com/embed/uI7J3II59lc?si=2kfAND6fQjvSYMBD&amp;start=360",
       },
       {
-        id: "implications-collaborative",
-        title: "Collaborative design possibilities",
-        url: "https://medium.com/@nolac/reflections-on-dynamicland-65158b06196#:~:text=Collaborative%20design%20possibilities",
+        id: "cellular-playgrounds",
+        title: "Wetenschap: Cellular playgrounds",
+        url: "https://www.youtube.com/embed/nT4E5HkpLjo?si=DlscFNK_CFVzqWla&amp;start=387",
       },
     ],
   },
   {
-    id: "applications",
-    title: "Potential Applications",
-    url: "https://www.youtube.com/embed/fsF7enQY8uI",
+    id: "research",
+    title: "Research",
+    url: "https://dynamicland.org/2017/Zine/",
     subtopics: [
       {
-        id: "applications-spaces",
-        title: "Interactive spaces",
-        url: "https://www.youtube.com/embed/fsF7enQY8uI?start=60",
-      },
-      {
-        id: "applications-education",
-        title: "Education and learning environments",
-        url: "https://www.youtube.com/embed/fsF7enQY8uI?start=180",
-      },
-      {
-        id: "applications-prototyping",
-        title: "Prototyping and ideation tools",
-        url: "https://www.youtube.com/embed/fsF7enQY8uI?start=300",
-      },
-    ],
-  },
-  {
-    id: "challenges",
-    title: "Challenges and Limitations",
-    url: "https://breadboard.me/how-we-can-move-beyond-dynamicland/",
-    subtopics: [
-      {
-        id: "challenges-technical",
-        title: "Technical hurdles",
-        url: "https://breadboard.me/how-we-can-move-beyond-dynamicland/#technical-hurdles",
-      },
-      {
-        id: "challenges-adoption",
-        title: "Adoption and scaling issues",
-        url: "https://breadboard.me/how-we-can-move-beyond-dynamicland/#adoption-and-scaling",
-      },
-    ],
-  },
-  {
-    id: "future",
-    title: "Future Outlook",
-    url: "https://www.youtube.com/embed/Njvf27R3XBU",
-    subtopics: [
-      {
-        id: "future-impact",
-        title: "Potential impact on design industry",
-        url: "https://www.youtube.com/embed/Njvf27R3XBU?start=60",
-      },
-      {
-        id: "future-influence",
-        title: "Influence on future design tools",
-        url: "https://www.youtube.com/embed/Njvf27R3XBU?start=180",
-      },
-    ],
-  },
-  {
-    id: "conclusion",
-    title: "Conclusion",
-    url: "https://dynamicland.org/get-involved/",
-    subtopics: [
-      {
-        id: "conclusion-takeaways",
-        title: "Key takeaways for designers",
-        url: "https://dynamicland.org/get-involved/#takeaways",
-      },
-      {
-        id: "conclusion-qa",
-        title: "Q&A session",
-        url: "https://dynamicland.org/get-involved/#faq",
+        id: "dynamicland-concept",
+        title: "Research agenda & visie (2014)",
+        url: "https://dynamicland.org/2014/CDG_Research_Agenda_poster.pdf",
       },
     ],
   },
@@ -170,11 +83,15 @@ const TopicItem = ({
   return (
     <button
       onClick={() => setSelectedTopic(topic)}
-      className={`w-full text-left py-1 px-2 rounded text-sm ${
+      className={`text-left py-1 px-2 rounded ${
         selectedTopic.id === topic.id
-          ? "bg-blue-500 text-white"
-          : "hover:bg-gray-200"
-      } ${isSubtopic ? "ml-4" : "font-semibold"}`}
+          ? "bg-purple text-black"
+          : "text-white hover:bg-black"
+      } ${
+        isSubtopic
+          ? "ml-4 text-sm subtopic-button"
+          : "text-md font-semibold uppercase "
+      }`}
     >
       {topic.title}
     </button>
@@ -186,8 +103,10 @@ const DynamiclandPresentation = () => {
 
   return (
     <div className="flex h-screen">
-      <div className="w-1/4 bg-gray-100 p-4 overflow-y-auto">
-        <h2 className="text-lg font-bold mb-2">Dynamicland Presentation</h2>
+      <div className="w-1/4 sidebar p-4 overflow-y-auto">
+        <h2 className="text-xl font-bold mb-2 text-purple-norday uppercase">
+          Dynamicland highlights
+        </h2>
         <div className="space-y-1">
           {topics.map((topic) => (
             <div key={topic.id}>
@@ -210,12 +129,11 @@ const DynamiclandPresentation = () => {
         </div>
       </div>
       <div className="w-3/4 p-4">
-        <h3 className="text-xl font-bold mb-4">{selectedTopic.title}</h3>
         <iframe
           src={selectedTopic.url}
           height={700}
           title={selectedTopic.title}
-          className="w-full h-5/6 border-2 border-gray-300 rounded"
+          className="w-full h-full border-2 border-gray-300 rounded"
           allowFullScreen
         ></iframe>
       </div>
